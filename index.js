d680c4530ee764b8e1a93d07e86eee1efb40c9b6
Bryanstuck <38543113+Bryanstuck@users.noreply.github.com>
2018-05-22 18:03:05 -0400

    edit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 <script>
 function myFunction() {
-    alert("Welcome to our Cancer Awareness Page, feel free to browse the links we've provided and donate to a cause today!");
+    alert("We hope you enjoyed your stay at our Cancer Awareness Page, feel free to browse the links we've provided and donate to a cause today!");
 }
 </script>
