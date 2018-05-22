93399195bf1f62d46c3815e81443fa99a62d7554
Bryanstuck <38543113+Bryanstuck@users.noreply.github.com>
2018-05-22 18:16:27 -0400

    k

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-<script>
 function myFunction() {
     alert("We hope you enjoyed your stay at our Cancer Awareness Page, feel free to browse the links we've provided and donate to a cause today!");
 }
-</script>
