e3d8ef6be9a0bc303bc6833ed4f617536f0c055d
Bryanstuck <38543113+Bryanstuck@users.noreply.github.com>
2018-05-24 20:09:56 -0400

    fix

--- Group Project/README.md
+++
@@ -1,1 +0,0 @@
-# bryanstuck.github.io
--- Group Project/index.js
+++
@@ -1,3 +0,0 @@
-function myFunction() {
-    alert("We hope you enjoyed your stay at our Cancer Awareness Page, feel free to browse the links we've provided and donate to a cause today! Thanks for visiting!");
-}
